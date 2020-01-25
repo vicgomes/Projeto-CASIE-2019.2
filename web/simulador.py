9cbcfcef742c89c14c09c72e84bdbb976b5e2b53
Victor Emanuel <55245500+vicgomes@users.noreply.github.com>
2020-01-25 11:21:34 -0300

    Delete simulador.py

--- web/simulador.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-while(True):
-   valor = int(input("Informe um valor: "))
-   r = requests.post("http://casie.herokuapp.com/upload", json={"LUMINOSIDADE":luminosidade})
-   print(r.status_code)
